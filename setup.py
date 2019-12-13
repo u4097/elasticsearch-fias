5f251f6c13fbe0d0b97f2a0c70bde9744d197fdb
sitnikov <oleg.sitnikov@icloud.com>
2019-12-13 23:51:15 +0300

    Add changelog

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
 
-with open ("README.md", "r") as fh:
+with open ("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="fias-elasticsearch",
-    version="0.0.1",
+    version="1.4.0",
     author="Oleg Sitnikov",
     author_email="oleg.sitnikov@icloud.com",
     description="Download fias XML base, import it in Elasticsearch, and update",
     long_description=long_description,
-    url="https://github.com/u4097/fias-update",
+    url="https://github.com/u4097/elasticsearch-fias",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
